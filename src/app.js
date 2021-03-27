02c776df1fb522a925e3b23515bcd01a8b340221
Harisankar G <harisankarg909@gmail.com>
2021-03-27 12:27:51 +0530

    initial working without scss

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/styles.css'
+
+console.log("starting. .....");
+const template = (
+    <div>
+        <h1>Page Title</h1>
+        <p>This is my page</p>
+    </div>
+);
+
+ReactDOM.render(template, document.getElementById('app'));
